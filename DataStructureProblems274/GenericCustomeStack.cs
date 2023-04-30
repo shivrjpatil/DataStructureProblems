c22c85951de74ea1c9785e597c07fd0e184ad905
shivrjpatil <shivrajp972@gmail.com>
2023-04-30 19:02:25 +0530

    Added Generic class Custom Linked List

---
+++ DataStructureProblems274/GenericCustomeStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructureProblems274
+{
+    internal class GenericCustomeStack<T>
+    {
+            public Node<T> top;
+            public GenericCustomeStack()
+            {
+                this.top = null;
+            }
+            public void Push(T value)
+            {
+                Node<T> node = new Node<T>(value);
+                if (this.top == null)
+                {
+                    node.Next = null;
+                }
+                else
+                {
+                    node.Next = this.top;
+                }
+                this.top = node;
+                Console.WriteLine("{0} pushed to stack", value);
+            }
+            public void Peek()
+            {
+                if (this.top == null)
+                {
+                    Console.WriteLine("Stack is Empty");
+                    return;
+                }
+                Console.WriteLine("{0} is in the top of the stack", this.top.data);
+            }
+            public void Pop()
+            {
+                if (this.top == null)
+                {
+                    Console.WriteLine("Stack is Empty");
+                    return;
+                }
+                Console.WriteLine("Value Popped is {0}", this.top.data);
+                this.top = this.top.Next;
+            }
+
+            public void Display()
+            {
+                Node<T> temp = this.top;
+                Console.WriteLine("Stack is as:");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.Next;
+                }
+            }
+        }
+    }
+
+
--- DataStructureProblems274/Program.cs
+++ DataStructureProblems274/Program.cs
@@ -10,16 +10,34 @@ namespace DataStructureProblems274
     {
         static void Main(string[] args)
         {
-            GenericClassCustomLinkedList<int> linkedList = new GenericClassCustomLinkedList<int>();
-            linkedList.AddNode(34);
-            linkedList.AddNode(56);
-            linkedList.AddNode(67);
-            linkedList.AddReverseData(34);
-            linkedList.AddReverseData(56);
-            linkedList.AddReverseData(67);
-            linkedList.dispaly();
-            linkedList.PopLast();
-            linkedList.dispaly();
+            Console.WriteLine("1.Linked List\n2.Stack");
+            int option = Convert.ToInt32(Console.ReadLine());
+           switch (option)
+           {
+                case 1:
+                GenericClassCustomLinkedList<int> linkedList = new GenericClassCustomLinkedList<int>();
+                linkedList.AddNode(34);
+                linkedList.AddNode(56);
+                linkedList.AddNode(67);
+                linkedList.AddReverseData(34);
+                linkedList.AddReverseData(56);
+                linkedList.AddReverseData(67);
+                linkedList.dispaly();
+                linkedList.PopLast();
+                linkedList.dispaly();
+                break;
+                case 2:
+                    GenericCustomeStack<int> stack = new GenericCustomeStack<int>();
+                    stack.Push(23);
+                    stack.Push(24);
+                    stack.Push(35);
+                    stack.Display();
+                    stack.Peek();
+                    stack.Pop();
+                    stack.Pop();
+                    stack.Display();
+                    break;
+           }
             Console.ReadLine();
         }
     }
